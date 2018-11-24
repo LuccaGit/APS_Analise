d6218ff83996e5457c96650c93e10f6ad720b053
gabrields1998 <gabrieldavidsacca@hotmail.com>
2018-11-24 16:33:50 -0200

    bubbleSort finalizado

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,19 +1,41 @@
 #include "vetor.h"
+#define TAMANHO 1000
 
+
+void bubbleSort(int *vet,int tamanho) {
+    printf("---------------------------bubbleSort----------------------------------\n\n");
+    double tempoIni = (double) clock() / 1000000;
+    int aux, count = 0;
+    for(int i = 0; i < tamanho; i++){
+        for(int j = 0; j < tamanho; j++){
+            if(vet[i] < vet[j]){
+                count++;
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
+        }
+    }
+
+    double tempoFim = (double) clock () / 1000000;
+    float tempo = tempoFim - tempoIni;
+
+    printf("tempo de execucao = %f Segundos\n",tempo);
+    printf("quantidade de instrucoes = %d Instrucoes\n",count);
+    printf("\n---------------------------bubbleSort----------------------------------\n");
+}
 void main(int argc, char *argv[]) {
-
     FILE *arquivo;
     arquivo = fopen(argv[1], "r");
-    int count = 0;
     if(arquivo == NULL) {
         printf("arquivo nao encontrado \n");
         exit(0);
     }
 
-    int vetor[1000], i = 0;
-    char caracter[20];
-    
+    int vetor[TAMANHO], i = 0, count;
+
     iniciaVetor(vetor,arquivo);
-        
+    bubbleSort(vetor,TAMANHO);
     fclose(arquivo);
+
 }
--- vetor.h
+++ vetor.h
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 void iniciaVetor(int *vet, FILE *arquivo){
     int i = 0;
     char caracter[20];
     
     while (fgets(caracter, 20, arquivo) != NULL){
-        printf("caracter = %s ", caracter);
         vet[i] = atoi(caracter);
-        printf("inteiro = %d \n", vet[i]);
         i++;
     }
 }
